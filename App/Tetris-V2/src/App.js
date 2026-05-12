90398dec7caa8bb2cc8c5b8b764491724184bb98
Natthida Bunsuea - 6609650350 <145255361+NatthidaBunsuea@users.noreply.github.com>
2026-05-13 01:20:05 +0700

    Rollback test1

--- App/Tetris-V2/src/App.js
+++ App/Tetris-V2/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 
-import Tetris from "./components/Tetris";
+import Tetris from "./components/Tetris1";
 
 export default function App() {
   return <Tetris />;
