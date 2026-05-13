40ac28a53a04ed3949e64af18095e53cd84008a6
Natthida Bunsuea - 6609650350 <145255361+NatthidaBunsuea@users.noreply.github.com>
2026-05-13 13:16:22 +0700

    Update Tetris component import path

--- App/Tetris-V2/src/App.js
+++ App/Tetris-V2/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 
-import Tetris from "./components/Tetris1";
+import Tetris from "./components/Tetris";
 
 export default function App() {
   return <Tetris />;
